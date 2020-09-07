f7a93d3729bb477528ea6ced6a19a098d0e34a44
Steven Riady <riadysteve@gmail.com>
2020-09-07 15:25:21 +0700

    Cleanup homepage link

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>My name is Steven Riady. I'm a Front-end Web Developer.</h2>
-      <p>
-        Need a developer ? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
